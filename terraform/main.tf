7102daed64e89f8b84357981f4460e2f5adf933f
Eliud Kinyanjui <eliud.kinyanjui@ramani.co.ke>
2023-07-30 23:15:47 +0300

    Stage 2 - Terraform setup

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+provider "local" {}
+
+resource "local_file" "ssh_config" {
+  filename = "${path.module}/ssh_config"
+  content = <<-EOT
+    Host vagrant
+      HostName 127.0.0.1
+      Port 2222
+      User vagrant
+      StrictHostKeyChecking no
+      PasswordAuthentication no
+      IdentityFile "${path.module}/.vagrant/machines/default/virtualbox/private_key"
+  EOT
+}
+
+resource "null_resource" "vagrant" {
+  provisioner "local-exec" {
+    command = "vagrant init geerlingguy/ubuntu2004 && vagrant up"
+    working_dir = "${path.module}"
+  }
+}
+
